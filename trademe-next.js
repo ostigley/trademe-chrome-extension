d11db8f6e6bdb3149cbc7410e0bc447c9e30b296
Oliver Stigley <oliverstigley@gmail.com>
2016-07-04 11:20:54 +1200

    working prototype

--- trademe-next.js
+++ trademe-next.js
@@ -1,1 +1,22 @@
-$('body').prepend('<p>Hello world</p>')
+var prevResults = localStorage.getItem('trademeSearchResults').split(',')
+
+pageUrl = $(location).attr('href')
+const isSearchUrl = pageUrl.includes('SearchResults.aspx?searchString')
+
+if (isSearchUrl) {
+	var results = []
+	for (i=1; i<5; i++) {
+		let tile = $(`a.tile-${i}`)
+		let length = tile.length
+		for(k=0; k<length; k++){
+			results.push(tile[k].href)
+		}
+	}
+	localStorage.setItem('trademeSearchResults',results)
+} else if (prevResults.indexOf(pageUrl) > -1) {
+	console.log('Your in a result url')
+	var nextUrl = prevResults[prevResults.indexOf(pageUrl)+1]
+	$('.banner-content').append(`<a href=${nextUrl} >View next in search results</a>`)
+}
+
+
