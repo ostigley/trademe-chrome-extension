4b79786f3d447d9151bb3f5bdee16a0d64ed0cbc
Oliver Stigley <oliverstigley@gmail.com>
2016-07-04 13:57:44 +1200

    updated results object

--- trademe-next.js
+++ trademe-next.js
@@ -1,23 +1,29 @@
-var prevResults = localStorage.getItem('trademeSearchResults').split(',')
+var prevResults = JSON.parse(localStorage.getItem('trademeSearchResults'))
+console.log(prevResults)
+var results = prevResults.results ? prevResults.results : null
 
-pageUrl = window.location.href
+const pageUrl = window.location.href
 const isSearchUrl = pageUrl.includes('SearchResults.aspx?searchString')
 
+
 if (isSearchUrl) {
-	var results = []
+	var searchData = {
+		"url": pageUrl,
+		"results": []
+	}
 	let tile = document.querySelectorAll('a.tile-1, a.tile-2, a.tile-3, a.tile-4')
 	let length = tile.length
 	for(k=0; k<length; k++){
-		results.push(tile[k].href)
+		searchData["results"].push(tile[k].href)
 	}
-	localStorage.setItem('trademeSearchResults',results)
-} else if (prevResults.indexOf(pageUrl) > -1) {
-	var nextUrl = prevResults[prevResults.indexOf(pageUrl)+1]
+	localStorage.setItem('trademeSearchResults', JSON.stringify(searchData))
+} else if (results.indexOf(pageUrl) > -1) {
+	var nextUrl = results[results.indexOf(pageUrl)+1]
 	var button = document.createElement('button')
-	button.setAttribute('id', 'Watchlist_SaveToWatchlistButton')
+	button.setAttribute('id', 'olly')
 	button.setAttribute('style', 'width:250px')
 	button.innerHTML = `<a href=${nextUrl} >View next in search results</a>`
-	document.querySelector('.banner-content').appendChild(button)
+	document.querySelector('.listingBreadCrumbs').appendChild(button)
 }
 
 
