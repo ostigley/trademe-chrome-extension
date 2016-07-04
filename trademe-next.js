e076d0e175d78f332e25d667e6d9ecf72a40564d
Oliver Stigley <oliverstigley@gmail.com>
2016-07-04 12:02:13 +1200

    removed jquery

--- trademe-next.js
+++ trademe-next.js
@@ -1,25 +1,23 @@
 var prevResults = localStorage.getItem('trademeSearchResults').split(',')
 
-pageUrl = $(location).attr('href')
+pageUrl = window.location.href
 const isSearchUrl = pageUrl.includes('SearchResults.aspx?searchString')
 
 if (isSearchUrl) {
 	var results = []
-	let tile = $('a.tile-1, a.tile-2, a.tile-3, a.tile-4')
+	let tile = document.querySelectorAll('a.tile-1, a.tile-2, a.tile-3, a.tile-4')
 	let length = tile.length
 	for(k=0; k<length; k++){
-	console.log(tile[k].href)
 		results.push(tile[k].href)
 	}
-	
 	localStorage.setItem('trademeSearchResults',results)
 } else if (prevResults.indexOf(pageUrl) > -1) {
-	console.log('Your in a result url')
 	var nextUrl = prevResults[prevResults.indexOf(pageUrl)+1]
-	$('.banner-content').append(
-		`<button style="width:250px" id="Watchlist_SaveToWatchlistButton">
-			<a href=${nextUrl} >View next in search results</a>
-		</button>`)
+	var button = document.createElement('button')
+	button.setAttribute('id', 'Watchlist_SaveToWatchlistButton')
+	button.setAttribute('style', 'width:250px')
+	button.innerHTML = `<a href=${nextUrl} >View next in search results</a>`
+	document.querySelector('.banner-content').appendChild(button)
 }
 
 
