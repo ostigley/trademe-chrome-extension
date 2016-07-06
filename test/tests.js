849e923a52633337ff2719e5639e1e479dfb1c3a
Oliver Stigley <oliverstigley@gmail.com>
2016-07-06 15:11:33 +1200

    nightwatch configured for chrome

--- test/tests.js
+++ test/tests.js
@@ -4,8 +4,12 @@ module.exports = {
   'fill in a form' : function (browser) {
     browser
       .url('http://www.trademe.co.nz')
-      .waitForElementVisible('body', 3000)
-      .pause(1000)
+      .waitForElementVisible('body',500)
+      .setValue('input[id=searchString]', ['rugby balls', browser.Keys.ENTER])
+      .pause('3000')
+      .click(".tile-4")
+      .pause('3000')
+      
       .end()
   },
 
